4626802a70ec3f5c6d53c34b964f7ab6e7fbb7fa
Ne_Plater <dragonsthreeheads@gmail.com>
2022-02-20 17:10:14 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let storage = []
+
+  function FindPair(elem) {
+    return bracketsConfig.find(x =>
+      x.includes(elem)
+    )
+  }
+
+
+  for (let element of str) {
+    let pair = FindPair(element);
+
+    if (element === pair[1]) {
+      if (storage[0] === pair[0]) {
+        storage.shift(storage[0]);
+      } else if (pair[0] === pair[1]) {
+        storage.unshift(element);
+
+      } else {
+        return false;
+      }
+    } else {
+      storage.unshift(element);
+    }
+
+
+  }
+
+  return storage.length === 0
 }
+
